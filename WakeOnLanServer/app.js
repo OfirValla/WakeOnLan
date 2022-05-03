50b0db1bceca68e2ea734fbb9b03b57780fe2b1b
Ofir Valla <ofirvalla50@gmail.com>
2022-05-03 23:15:20 +0300

    Initial commit

---
+++ WakeOnLanServer/app.js
@@ -0,0 +1,26 @@
+import { initializeApp, cert } from 'firebase-admin/app';
+import { getDatabase } from 'firebase-admin/database';
+import * as wol from 'wol';
+import * as fs from 'fs';
+
+const serviceAccount = JSON.parse(fs.readFileSync(process.env.FIREBASE_SERVICE_ACCOUNT));
+const app = initializeApp({
+    credential: cert(serviceAccount),
+    databaseURL: process.env.FIREBASE_DATABASE_URL
+});
+
+const db = getDatabase(app);
+db.ref('wake-on-lan').on('value', (snapshot) => {
+    const changes = snapshot.val();
+    if (!changes) return;
+
+    for (const computerName of Object.keys(changes)) {
+        console.log(`Sending wake-on-lan for: ${computerName}`);
+        
+        const macAddress = changes[computerName].replaceAll('-', ':');
+        wol.wake(macAddress, function (err, res) { });
+
+        // Remove the keys after waking on lan the computers
+        db.ref(`wake-on-lan/${computerName}`).remove()
+    }
+});
