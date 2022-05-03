de98712761e8c8239abb996b0682d94a7a2df410
Ofir Valla <ofirvalla50@gmail.com>
2022-05-04 00:27:14 +0300

    building the service account object

--- WakeOnLanServer/app.js
+++ WakeOnLanServer/app.js
@@ -2,12 +2,18 @@ import { initializeApp, cert } from 'firebase-admin/app';
 import { getDatabase } from 'firebase-admin/database';
 import * as wol from 'wol';
 
-//import * as fs from 'fs';
-// const serviceAccount = JSON.parse(fs.readFileSync(process.env.FIREBASE_SERVICE_ACCOUNT));
-
-const buff = new Buffer(process.env.FIREBASE_SERVICE_ACCOUNT);
-const serviceAccount = JSON.parse(buff.toString('utf-8'));
-
+const serviceAccount = {
+    "type": "service_account",
+    "project_id": process.env.PROJECT_ID,
+    "private_key_id": process.env.PRIVATE_KEY_ID,
+    "private_key": process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "client_email": process.env.CLIENT_EMAIL,
+    "client_id": process.env.CLIENT_ID,
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL
+}
 const app = initializeApp({
     credential: cert(serviceAccount),
     databaseURL: process.env.FIREBASE_DATABASE_URL
